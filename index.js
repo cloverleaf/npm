95647f61b18cfbb575c9cf759d479d159586c84d
Allie Law <13716824+ChildishGiant@users.noreply.github.com>
2022-08-23 19:24:20 +0100

    feat: :sparkles: add support for custom presets (dynamically passed)

--- index.js
+++ index.js
@@ -21,9 +21,10 @@ export const possibleRequirements = {
  * @param {string} masterPass
  * @param {boolean} [presetToggle=false] True if we want to use a preset
  * @param {number} [length=16] int - Desired length of the password
+ * @param {object} [customPreset] JSON object to use as a custom preset
  * @returns {string} Output password
  */
-export function generate (appName, masterPass, presetToggle = false, length = defaultLength) {
+export function generate (appName, masterPass, presetToggle = false, length = defaultLength, customPreset = {} ) {
   // Ensure types
   if (typeof appName !== 'string') throw new Error('App name must be a string')
   if (appName.length < 1) throw new Error('App name must be more than 0 characters')
@@ -37,7 +38,7 @@ export function generate (appName, masterPass, presetToggle = false, length = de
   let chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" // Defualt character set (Set here but overwritten if there's a custom one.)
   const requirements = [] // By default we have no requirements but reset it so we don't carry them over
   let regex // Blank for the same reason
-  appName = appName.trim()
+  appName = appName.trim() // Remove trailing and leading spaces on app name
   length = Math.trunc(length) // Get the desired length of a password and make sure it's an integer
   let result = '' // Has to be here, not in the loop for scope purposes
 
@@ -56,39 +57,45 @@ export function generate (appName, masterPass, presetToggle = false, length = de
       throw new Error('Invalid preset')
     }
 
+    let site = siteData[appName]
+
+    // If it's a custom preset
+    if (Object.keys(customPreset) != 0) {
+      site = customPreset
+    }
+
     // If it has a custom minLength
-    if ('minLength' in siteData[appName]) {
+    if ('minLength' in site) {
       // Replace the default minLength with the supplied one.
-      minLength = siteData[appName].minLength
+      minLength = site.minLength
     }
 
     // If it has a custom maxLength
-    if ('maxLength' in siteData[appName]) {
+    if ('maxLength' in site) {
       // Replace the default maxLength with the supplied one.
-      maxLength = siteData[appName].maxLength
+      maxLength = site.maxLength
     }
 
     // If it has a custom character set
-    if ('chars' in siteData[appName]) {
+    if ('chars' in site) {
       // Replace the default character set with the supplied one.
-      chars = siteData[appName].chars
+      chars = site.chars
     }
 
     // If it has a regex
-    if ('regex' in siteData[appName]) {
+    if ('regex' in site) {
       // Set the regex to match
       try {
-        regex = new RegExp(siteData[appName].regex)
+        regex = new RegExp(site.regex)
       } catch (SyntaxError) {
-        throw new Error(`Invalid regex from ${appName} "${siteData[appName].regex}"`)
+        throw new Error(`Invalid regex from ${appName} "${site.regex}"`)
       }
     }
 
-    if ('requirements' in siteData[appName]) {
-      // TODO This could be a map
-      for (let i = 0; i < siteData[appName].requirements.length; i++) {
+    if ('requirements' in site) {
+      for (let i = 0; i < site.requirements.length; i++) {
         requirements.push(
-          possibleRequirements[siteData[appName].requirements[i]]
+          possibleRequirements[site.requirements[i]]
         )
       }
     }
